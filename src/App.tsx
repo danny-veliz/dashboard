4166bfac01cbd549b48c8c0f212763838d044b48
medardomoranb <119766136+medardomoranb@users.noreply.github.com>
2024-11-27 10:13:15 -0500

    modifed

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,19 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-                <Grid>
+                <Grid size={{ xs: 12, sm: 3 }}>
                  <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
-             </Grid>
+             </Grid >
 
-             <Grid >
+             <Grid size={{ xs: 12, sm: 3 }} >
                  <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
              </Grid>
                 
-             <Grid >
+             <Grid size={{ xs: 12, sm: 3 }}>
                  <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
              </Grid>
                 
-             <Grid>
+             <Grid size={{ xs: 12, sm: 3 }}>
                  <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
              </Grid>
       
